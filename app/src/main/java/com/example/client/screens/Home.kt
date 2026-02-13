8147c4cca8a87226d9e4369c71c48af4a5b94f12
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2026-02-13 20:50:59 +0530

    added Home page

--- app/src/main/java/com/example/client/screens/Home.kt
+++ app/src/main/java/com/example/client/screens/Home.kt
@@ -5,14 +5,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -27,29 +27,25 @@ fun Home() {
         Column(
             modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
         ) {
-            Text(
-                text = "Home",
-                style = MaterialTheme.typography.displayMedium,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-            
-            Text(
-                text = "Welcome to PH Health Checker",
-                style = MaterialTheme.typography.bodyLarge,
-                textAlign = TextAlign.Center,
-                color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f),
-                modifier = Modifier.padding(top = 16.dp)
-            )
+            Button(onClick = { },
+                Modifier.width(80.dp).height(40.dp)
+                ) {
+                Text("Rent Your Space")
+            }
+
+            Button(onClick = { },
+                Modifier.width(80.dp).height(40.dp)
+            ) {
+                Text("Find your parking place")
+            }
         }
     }
 }
 
 @Preview
 @Composable
-fun Home1() {
-   Home()
+fun HomePreview() {
+    Home()
 }
